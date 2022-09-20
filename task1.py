fb6df0582c967fd2951097731ddad32fa7bfb112
VladimirKravtsov36 <vladimir140201@yandex.ru>
2022-09-20 16:43:20 +0300

    correct according requirements

--- task1.py
+++ task1.py
@@ -9,8 +9,8 @@ class CountVectorizer:
             self._vocabulary.update(doc.lower().split())
 
         termdoc_matr = [[0 for _ in self._vocabulary] for _ in corpus]
-        
-        for i, doc in enumerate(corpus):  
+
+        for i, doc in enumerate(corpus):
             for j, feature in enumerate(self._vocabulary):
                 termdoc_matr[i][j] = doc.lower().split().count(feature)
 
@@ -19,12 +19,14 @@ class CountVectorizer:
     def get_feature_names(self):
         return list(self._vocabulary)
 
-corpus = [
-    'Crock Pot Pasta Never boil pasta again',
-    'Pasta Pomodoro Fresh ingredients Parmesan to taste'
-]
+if __name__ == 'main':
 
-vectorizer = CountVectorizer()
-count_matrix = vectorizer.fit_transform(corpus)
-print(count_matrix)
-print(vectorizer.get_feature_names())
+    corpus = [
+        'Crock Pot Pasta Never boil pasta again',
+        'Pasta Pomodoro Fresh ingredients Parmesan to taste'
+    ]
+
+    vectorizer = CountVectorizer()
+    count_matrix = vectorizer.fit_transform(corpus)
+    print(count_matrix)
+    print(vectorizer.get_feature_names())
